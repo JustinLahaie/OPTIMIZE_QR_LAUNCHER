2be4d31ea13f28599c0aefce638ac25866b559b8
JustinLahaie <kitchensbyhillcrest@gmail.com>
2025-03-28 11:56:55 -0400

    Update all files with local changes

--- QRCodeScanner/obj/Debug/net8.0-windows/QRCodeScanner.AssemblyInfo.cs
+++ QRCodeScanner/obj/Debug/net8.0-windows/QRCodeScanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QRCodeScanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18fade22ed3bc51fdda4f66f0dcdb5d88a416c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("QRCodeScanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QRCodeScanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
